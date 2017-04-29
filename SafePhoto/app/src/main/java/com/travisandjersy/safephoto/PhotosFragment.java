2df02cb32f0e99ab02dee50644d9e2325af6349b
trvslhlt <trvslhlt@gmail.com>
2017-04-29 13:16:01 -0400

    show list in photos
    - loading local list of strings

--- SafePhoto/app/src/main/java/com/travisandjersy/safephoto/PhotosFragment.java
+++ SafePhoto/app/src/main/java/com/travisandjersy/safephoto/PhotosFragment.java
@@ -2,29 +2,42 @@ package com.travisandjersy.safephoto;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 /**
  * Created by trvslhlt on 4/29/17.
  */
 
-public class PhotosFragment extends android.support.v4.app.Fragment {
+public class PhotosFragment extends ListFragment implements OnItemClickListener{
 
     private static final String TAG = "PhotosFragment";
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-//        return super.onCreateView(inflater, container, savedInstanceState);
         return inflater.inflate(R.layout.photos_view, container, false);
     }
 
     @Override
-    public void onDestroy() {
-        Log.d(TAG, "destroyed");
-        super.onDestroy();
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+                getActivity(),
+                R.array.SamplePhotoNames,
+                android.R.layout.simple_expandable_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
     }
 }
